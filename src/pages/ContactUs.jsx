88eb1976b295f021aeef9170169795713a5171e4
Kumarsam17 <50691250+Kumarsam17@users.noreply.github.com>
2023-08-18 10:25:52 +0530

    contact page content change

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -4,7 +4,11 @@ import'../stylesheets/contactus.css'
 function ContactUs() {
   return (
     <div className="main">
-      <div className="containers">details</div>
+      <div className="containers">
+        <span id="name">Pintu Kumar</span>
+        <span className="email">xyzlovers35@gmail.com</span>
+
+      </div>
       <div className="containers">details</div>
       <div className="containers">details</div>
     </div>
